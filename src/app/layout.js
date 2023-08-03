39b64f293c023b877c12959a8a19b7bc425d4f7d
nutrasoul <141176261+nutrasoul@users.noreply.github.com>
2023-08-03 19:16:28 +0000

    tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,7 +12,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Head>
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KG49DJZJ');</script>
+
+      </Head>
+      <body className={inter.className}>{children}
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KG49DJZJ"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+
+      </body>
     </html>
   )
 }
