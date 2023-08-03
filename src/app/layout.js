d8ea12698305b56a6fa8988d33b19ea5d2016790
nutrasoul <141176261+nutrasoul@users.noreply.github.com>
2023-08-03 16:36:12 -0300

    fix: tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import './globals.css'
+import Head from 'next/head'
+import Script from 'next/script'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,16 +15,18 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-      <script>(function(w,d,s,l,i){w[l]=w[l]||[]}w[l].push({'gtm.start'}
-new Date().getTime(),event:'gtm.js');var f=d.getElementsByTagName(s)[0],
+      
+      <Script strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];
+    w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+    var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f))(window,document,'script','dataLayer','GTM-KG49DJZJ');</script>
-
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KG49DJZJ')`}}></Script>
+     
       </Head>
       <body className={inter.className}>{children}
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KG49DJZJ"
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KG49DJZJ"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-
       </body>
     </html>
   )
